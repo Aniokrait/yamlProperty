d00c17fdd653d08333964c7eb7424e8b45b6addf
Aniokrait <itititcreclickhub@gmail.com>
2024-03-08 11:51:18 +0900

    initial commit

---
+++ src/main/java/io/github/aniokrait/yamlproperty/YamlpropertyApplication.java
@@ -0,0 +1,13 @@
+package io.github.aniokrait.yamlproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlpropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlpropertyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aniokrait/yamlproperty/YamlpropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aniokrait.yamlproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlpropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
