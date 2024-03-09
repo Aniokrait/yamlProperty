f1595b7d3701497d1c4c84d6e8ab6a66dfd23f76
Aniokrait <itititcreclickhub@gmail.com>
2024-03-09 11:56:51 +0900

    delete

--- src/main/java/io/github/aniokrait/yamlproperty/config/properties/ServerServletConfig.java
+++ src/main/java/io/github/aniokrait/yamlproperty/config/properties/ServerServletConfig.java
@@ -1,17 +1,14 @@
 package io.github.aniokrait.yamlproperty.config.properties;
 
-import io.github.aniokrait.yamlproperty.config.YamlPropertiesSourceFactory;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "server.servlet")
-//@PropertySource(value = {"classpath:/application.yml"}, factory = YamlPropertiesSourceFactory.class)
 @Getter
 @Setter
 public class ServerServletConfig {
